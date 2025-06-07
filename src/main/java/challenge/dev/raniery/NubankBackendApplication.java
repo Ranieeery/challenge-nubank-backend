9f53bdf119ce36ecac4bed64ea28d1b1b0a63e45
Ranieeery <raniery2003@hotmail.com>
2025-06-06 22:40:52 -0300

    feat: start nubank challenge with spring config

---
+++ src/main/java/challenge/dev/raniery/NubankBackendApplication.java
@@ -0,0 +1,13 @@
+package challenge.dev.raniery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NubankBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/challenge/dev/raniery/NubankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.dev.raniery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
