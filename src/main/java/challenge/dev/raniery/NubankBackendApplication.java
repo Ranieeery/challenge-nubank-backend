c8c30eb68039b922f0b4591cca314e9c67d8a10d
Ranieeery <raniery2003@hotmail.com>
2025-06-09 15:30:28 -0300

    feat: update pom.xml and application properties for PostgreSQL integration

--- src/main/java/challenge/dev/raniery/NubankBackendApplication.java
+++ src/main/java/challenge/dev/raniery/NubankBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NubankBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NubankBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NubankBackendApplication.class, args);
+    }
 
 }
